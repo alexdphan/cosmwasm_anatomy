a9267a836f2ba98ddb2118dad57b9bdd5146909d
alexdphan <alexphan0515@gmail.com>
2022-11-07 22:24:30 -0500

    notes

--- src/contract.rs
+++ src/contract.rs
@@ -6,11 +6,12 @@ use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response,
 use cw2::set_contract_version;
 
 use crate::error::ContractError;
-use crate::msg::{ExecuteMsg, GetCountResponse, InstantiateMsg, QueryMsg};
+// use crate::msg::{ExecuteMsg, GetCountResponse, InstantiateMsg, QueryMsg};
+use crate::msg::{InstantiateMsg, ExecuteMsg};
 use crate::state::{State, STATE};
 
 // version info for migration info
-const CONTRACT_NAME: &str = "crates.io:my-first-cosmwasm-contract";
+const CONTRACT_NAME: &str = "crates.io:cosmowasm_anatomy";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[cfg_attr(not(feature = "library"), entry_point)]
@@ -47,126 +48,157 @@ pub fn instantiate(
         .add_attribute("count", msg.count.to_string()))
 }
 
+// Defined in the file /src/contract.rs, the execute() function uses Rust's pattern matching to route the received ExecuteMsg to the appropriate handling logic, by either routing to the function try_increment() or try_reset() depending on the type of message received.
+// basically, the execute() function is a router that routes the received message to the appropriate function
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
-    deps: DepsMut,
-    _env: Env,
-    info: MessageInfo,
-    msg: ExecuteMsg,
+  deps: DepsMut,
+  _env: Env,
+  info: MessageInfo,
+  msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
-    match msg {
-        ExecuteMsg::Increment {} => execute::increment(deps),
-        ExecuteMsg::Reset { count } => execute::reset(deps, info, count),
-    }
+  match msg {
+    ExecuteMsg::Increment {} => try_increment(deps),
+    ExecuteMsg::Reset { count } => try_reset(deps, info, count),
+  }
 }
 
-pub mod execute {
-    use super::*;
+// It is quite straightforward to follow the logic of try_increment(). We acquire a mutable reference to the storage to update the item located at key "state", made accessible through the STATE convenience function defined in the src/state.rs. 
+// We then update the present state's count by returning an Ok result with the new state. Finally, we terminate the contract's execution with an acknowledgement of success by returning an Ok result with the default Response.
+pub fn try_increment(deps: DepsMut) -> Result<Response, ContractError> { // state is mutable and definied intially in state.rs
+  STATE.update(deps.storage, |mut state| -> Result<_, ContractError> {
+    state.count += 1;
+    Ok(state)
+  })?;
 
-    pub fn increment(deps: DepsMut) -> Result<Response, ContractError> {
-        STATE.update(deps.storage, |mut state| -> Result<_, ContractError> {
-            state.count += 1;
-            Ok(state)
-        })?;
+  Ok(Response::new().add_attribute("method", "try_increment"))
+}
+// In this example, the default Response is used for simplicity. However, the Response can be manually created to provide the following information:
+    // messages: A list of messages. This is how smart contracts execute other smart contract functions or use native modules.
+    // attributes: A list of key-value pairs to define emitted SDK events that can be subscribed to and parsed by clients.
+    // events: Extra, custom events separate from the main wasm one. These will have wasm- prepended to the type. These can be used by explorers and applications to report important events or state changes that occurred during the execution.
+    // data: additional data that the contract returns to the client.
 
-        Ok(Response::new().add_attribute("action", "increment"))
+// The logic for try_reset() is very similar to increment — except this time, we first check that the message sender is permitted to invoke the reset function. 
+// Please, observe the use of ContractError::Unauthorized {} to return an error if the sender is not the owner of the contract. Custom error messages can be defined in the file /src/error.rs.
+pub fn try_reset(deps: DepsMut, info: MessageInfo, count: i32) -> Result<Response, ContractError> {
+  STATE.update(deps.storage, |mut state| -> Result<_, ContractError> {
+    if info.sender != state.owner {
+      return Err(ContractError::Unauthorized {});
     }
-
-    pub fn reset(deps: DepsMut, info: MessageInfo, count: i32) -> Result<Response, ContractError> {
-        STATE.update(deps.storage, |mut state| -> Result<_, ContractError> {
-            if info.sender != state.owner {
-                return Err(ContractError::Unauthorized {});
-            }
-            state.count = count;
-            Ok(state)
-        })?;
-        Ok(Response::new().add_attribute("action", "reset"))
-    }
+    state.count = count;
+    Ok(state)
+  })?;
+  Ok(Response::new().add_attribute("method", "reset"))
 }
 
-#[cfg_attr(not(feature = "library"), entry_point)]
-pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
-    match msg {
-        QueryMsg::GetCount {} => to_binary(&query::count(deps)?),
-    }
-}
+// pub mod execute {
+//     use super::*;
 
-pub mod query {
-    use super::*;
+//     pub fn increment(deps: DepsMut) -> Result<Response, ContractError> {
+//         STATE.update(deps.storage, |mut state| -> Result<_, ContractError> {
+//             state.count += 1;
+//             Ok(state)
+//         })?;
 
-    pub fn count(deps: Deps) -> StdResult<GetCountResponse> {
-        let state = STATE.load(deps.storage)?;
-        Ok(GetCountResponse { count: state.count })
-    }
-}
+//         Ok(Response::new().add_attribute("action", "increment"))
+//     }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
-    use cosmwasm_std::{coins, from_binary};
+//     pub fn reset(deps: DepsMut, info: MessageInfo, count: i32) -> Result<Response, ContractError> {
+//         STATE.update(deps.storage, |mut state| -> Result<_, ContractError> {
+//             if info.sender != state.owner {
+//                 return Err(ContractError::Unauthorized {});
+//             }
+//             state.count = count;
+//             Ok(state)
+//         })?;
+//         Ok(Response::new().add_attribute("action", "reset"))
+//     }
+// }
 
-    #[test]
-    fn proper_initialization() {
-        let mut deps = mock_dependencies();
+// #[cfg_attr(not(feature = "library"), entry_point)]
+// pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
+//     match msg {
+//         QueryMsg::GetCount {} => to_binary(&query::count(deps)?),
+//     }
+// }
 
-        let msg = InstantiateMsg { count: 17 };
-        let info = mock_info("creator", &coins(1000, "earth"));
+// pub mod query {
+//     use super::*;
 
-        // we can just call .unwrap() to assert this was a success
-        let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
-        assert_eq!(0, res.messages.len());
+//     pub fn count(deps: Deps) -> StdResult<GetCountResponse> {
+//         let state = STATE.load(deps.storage)?;
+//         Ok(GetCountResponse { count: state.count })
+//     }
+// }
 
-        // it worked, let's query the state
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: GetCountResponse = from_binary(&res).unwrap();
-        assert_eq!(17, value.count);
-    }
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
+//     use cosmwasm_std::{coins, from_binary};
 
-    #[test]
-    fn increment() {
-        let mut deps = mock_dependencies();
+//     #[test]
+//     fn proper_initialization() {
+//         let mut deps = mock_dependencies();
 
-        let msg = InstantiateMsg { count: 17 };
-        let info = mock_info("creator", &coins(2, "token"));
-        let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
+//         let msg = InstantiateMsg { count: 17 };
+//         let info = mock_info("creator", &coins(1000, "earth"));
 
-        // beneficiary can release it
-        let info = mock_info("anyone", &coins(2, "token"));
-        let msg = ExecuteMsg::Increment {};
-        let _res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
+//         // we can just call .unwrap() to assert this was a success
+//         let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
+//         assert_eq!(0, res.messages.len());
 
-        // should increase counter by 1
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: GetCountResponse = from_binary(&res).unwrap();
-        assert_eq!(18, value.count);
-    }
+//         // it worked, let's query the state
+//         let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
+//         let value: GetCountResponse = from_binary(&res).unwrap();
+//         assert_eq!(17, value.count);
+//     }
 
-    #[test]
-    fn reset() {
-        let mut deps = mock_dependencies();
+//     #[test]
+//     fn increment() {
+//         let mut deps = mock_dependencies();
 
-        let msg = InstantiateMsg { count: 17 };
-        let info = mock_info("creator", &coins(2, "token"));
-        let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
+//         let msg = InstantiateMsg { count: 17 };
+//         let info = mock_info("creator", &coins(2, "token"));
+//         let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
 
-        // beneficiary can release it
-        let unauth_info = mock_info("anyone", &coins(2, "token"));
-        let msg = ExecuteMsg::Reset { count: 5 };
-        let res = execute(deps.as_mut(), mock_env(), unauth_info, msg);
-        match res {
-            Err(ContractError::Unauthorized {}) => {}
-            _ => panic!("Must return unauthorized error"),
-        }
+//         // beneficiary can release it
+//         let info = mock_info("anyone", &coins(2, "token"));
+//         let msg = ExecuteMsg::Increment {};
+//         let _res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
 
-        // only the original creator can reset the counter
-        let auth_info = mock_info("creator", &coins(2, "token"));
-        let msg = ExecuteMsg::Reset { count: 5 };
-        let _res = execute(deps.as_mut(), mock_env(), auth_info, msg).unwrap();
+//         // should increase counter by 1
+//         let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
+//         let value: GetCountResponse = from_binary(&res).unwrap();
+//         assert_eq!(18, value.count);
+//     }
 
-        // should now be 5
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: GetCountResponse = from_binary(&res).unwrap();
-        assert_eq!(5, value.count);
-    }
-}
+//     #[test]
+//     fn reset() {
+//         let mut deps = mock_dependencies();
+
+//         let msg = InstantiateMsg { count: 17 };
+//         let info = mock_info("creator", &coins(2, "token"));
+//         let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
+
+//         // beneficiary can release it
+//         let unauth_info = mock_info("anyone", &coins(2, "token"));
+//         let msg = ExecuteMsg::Reset { count: 5 };
+//         let res = execute(deps.as_mut(), mock_env(), unauth_info, msg);
+//         match res {
+//             Err(ContractError::Unauthorized {}) => {}
+//             _ => panic!("Must return unauthorized error"),
+//         }
+
+//         // only the original creator can reset the counter
+//         let auth_info = mock_info("creator", &coins(2, "token"));
+//         let msg = ExecuteMsg::Reset { count: 5 };
+//         let _res = execute(deps.as_mut(), mock_env(), auth_info, msg).unwrap();
+
+//         // should now be 5
+//         let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
+//         let value: GetCountResponse = from_binary(&res).unwrap();
+//         assert_eq!(5, value.count);
+//     }
+// }
--- src/msg.rs
+++ src/msg.rs
@@ -1,8 +1,10 @@
 // The file src/msg.rs is where different types of messages and responses the smart contract can receive and return are defined.
 
-use cosmwasm_schema::{cw_serde, QueryResponses};
+// use cosmwasm_schema::{cw_serde, QueryResponses};
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
 
-#[cw_serde]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
     pub count: i32,
 }
@@ -20,22 +22,47 @@ pub struct InstantiateMsg {
 
 // This message arrives embedded in the received MsgInstantiateContract message during instantiation.
 
-#[cw_serde]
+
+// Defined in the file /src/msg.rs, an ExecuteMsg is received when an address tries to invoke one of the smart contract functions through a MsgExecuteContract message. 
+// Unlike the InstantiateMsg, which was a single struct; the ExecuteMsg is an enumerator, which essentially holds a list of possible execute message structs with different names and attributes to account for the different types of functions that a smart contract can expose to a user. 
+// The execute() function demultiplexes these different types of messages and forwards them to the appropriate message handler logic.
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+// The line #[serde(rename_all = "snake_case")] performs a snake_case conversion (lowercase initials with an underscore between words) on the field names before serialization and deserialization. So, we'll have increment and reset instead of Increment and Reset when serializing and deserializing across JSON formatted messages.
 pub enum ExecuteMsg {
-    Increment {},
-    Reset { count: i32 },
+  Increment {},
+  Reset { count: i32 },
 }
+// The ExecuteMsg enum contains the different types of execute messages that our contract can understand.
+// At this point, our template contract can accept the following two types of execute messages in JSON format, embedded in a MsgExecuteContract message:
 
-#[cw_serde]
-#[derive(QueryResponses)]
-pub enum QueryMsg {
-    // GetCount returns the current count as a json-encoded number
-    #[returns(GetCountResponse)]
-    GetCount {},
-}
+// Increment
+    // Any address can utilize the Increment function to increment the current count by 1.
 
-// We define a custom struct for each query response
-#[cw_serde]
-pub struct GetCountResponse {
-    pub count: i32,
-}
+// {
+//   "increment": {}
+// }
+
+// Reset
+    // The owner of the contract can reset the count to an arbitrary number. The check regarding whether a user is the contract owner is a part of the execution logic.
+
+// {
+//   "reset": {
+//     "count": 5
+//   }
+// }
+
+
+// #[cw_serde]
+// #[derive(QueryResponses)]
+// pub enum QueryMsg {
+//     // GetCount returns the current count as a json-encoded number
+//     #[returns(GetCountResponse)]
+//     GetCount {},
+// }
+
+// // We define a custom struct for each query response
+// #[cw_serde]
+// pub struct GetCountResponse {
+//     pub count: i32,
+// }
